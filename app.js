5da7d752768fabc4ef42b517ba4e88f0fb5b3802
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-11 10:57:00 +0530

    taking input

---
+++ app.js
@@ -0,0 +1,7 @@
+var btnTranslate = document.querySelector("#btn-translate")
+btnTranslate.addEventListener("click",newfn)
+function newfn(){
+    console.log("clicked")
+}
+var txtInput=document.querySelector("#text-input")
+console.log(txtInput)
