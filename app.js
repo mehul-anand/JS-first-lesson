7122dd2a3364a4e7bff270d23a1d9ccc7684a1a4
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-13 08:22:16 +0530

    learning js input processing output

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-var btnTranslate = document.querySelector("#btn-translate")
-btnTranslate.addEventListener("click",newfn)
-function newfn(){
-    console.log("clicked")
-}
+var btnTranslate=document.querySelector("#btn-translate")
 var txtInput=document.querySelector("#text-input")
-console.log(txtInput)
+var outputDiv=document.querySelector("#output")
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    var text = serverURL + "?" + "text=" + input
+    console.log("text in fn="+text)
+    return text
+}
+
+function errorHandler(error){
+    console.log("An Error Occured",error)
+    alert("Something is not right , please try again later")
+}
+
+
+
+function clickHandler(){
+    var inputText=txtInput.value;
+    console.log("txtInput val="+inputText)
+    var newURl=getTranslationURL(inputText)
+    fetch(newURl)
+        .then(Response => Response.json())
+        .then(json => {
+            outputDiv.innerText=json.contents.translated
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click",clickHandler)
